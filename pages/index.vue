1b47170afe8cdc3405208e439acb6661deb9b8b4
Arthur Plazanet <plazanet.art@gmail.com>
2024-06-15 01:12:14 +0200

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <AwesomeSection>
+    <h2>
+      <a
+        href="https://github.com/arthur-plazanet/frontend-coding-challenge?tab=readme-ov-file#frontend-coding-challenge"
+        >README</a
+      >
+    </h2>
+
+    <img src="/img.png" alt="User avatar" height="300" />
+  </AwesomeSection>
+  <UsersList />
+</template>
